41fa9130090c72ffe2e13321ad413090b3166732
Feather <lisafeather@gmail.com>
2014-04-14 20:41:31 -0700

    basic addition works

--- calc.js
+++ calc.js
@@ -5,10 +5,12 @@ $(function() {
     var num1 = $("#number1").val();
     var num2 = $("#number2").val();
 
-    $("#resultsA").append(num1+ " + " +num2);
-    $("#resultsB").append(parseInt(num1) + parseInt(num2));
-
+    $("#resultsA").append(num1+ " + " +num2 + "</br>");
+    $("#resultsB").append(parseInt(num1) + parseInt(num2) + "</br>");
+    $("#number1").val("");
+    $("#number2").val("");
     e.preventDefault();
+
     
   })
   
